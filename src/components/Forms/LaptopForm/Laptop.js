ae0eb18cf567e92715d989f28c4a8c07bb976c43
Slomtadze <slomtadze90@gmail.com>
2022-09-03 16:13:07 +0400

    Styled custom Radio components

--- src/components/Forms/LaptopForm/Laptop.js
+++ src/components/Forms/LaptopForm/Laptop.js
@@ -3,10 +3,12 @@ import Input from "../../Inputs/Input";
 import Radio from "../../Inputs/Radio";
 import Select from "../../Inputs/Select";
 
+import styles from "./Laptop.module.css";
+
 const Laptop = (props) => {
   return (
     <Fragment>
-      <div className="box-style">
+      <div className={styles["box-style"]}>
         <Input
           styles="md"
           label="ლეპტოპის სახელი"
@@ -14,12 +16,18 @@ const Laptop = (props) => {
         />
         <Select array={[]} styles="md" />
       </div>
-      <div className="box-style-wrap">
+      <div className={styles["box-style-wrap"]}>
         <Select styles="sm" array={[]} />
-        <Input styles="sm" />
-        <Input styles="sm" />
-        <Input styles="md" />
-        <Radio styles="md" id="Label" name="Label" />
+        <Input styles="sm" label="CPU-ს ბირთვი" title="მხოლოდ ციფრები" />
+        <Input styles="sm" label="CPU-ს ნაკადი" title="მხოლოდ ციფრები" />
+        <Input styles="md" label="ლეპტოპის RAM (GB)" title="მხოლოდ ციფრები" />
+        <Radio styles="md" id="ssd" id2="hdd" title="მეხსიერების ბარათი" />
+        {/*         <Input
+          styles="md"
+          type="radio"
+          label="მეხსიერების ბარათი"
+          title="მხოლოდ ციფრები"
+        /> */}
       </div>
     </Fragment>
   );
--- src/components/Inputs/Radio.js
+++ src/components/Inputs/Radio.js
@@ -3,10 +3,12 @@ import styles from "./Radio.module.css";
 const Radio = (props) => {
   return (
     <div className={styles[props.styles]}>
-      <label htmlFor={props.id}>{props.name}</label>
-      <div className="default">
-        <input type="radio" id={props.id}></input>
-        <input type="radio" id={props.id}></input>
+      <p>{props.title}</p>
+      <div className={styles.default}>
+        <input type="radio" id={props.id} />
+        <label htmlFor={props.id}>{props.id.toUpperCase()}</label>
+        <input type="radio" id2={props.id2} />
+        <label>{props.id2.toUpperCase()}</label>
       </div>
     </div>
   );
